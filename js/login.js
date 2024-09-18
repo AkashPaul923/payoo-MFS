19f2f16c98f6d7bfd6560632b031e03abaa4248a
AkashPaul923 <akashpaul923@gmail.com>
2024-09-18 22:40:56 +0600

    add simple login handeller and prevent default behavior

---
+++ js/login.js
@@ -0,0 +1,11 @@
+// console.log("angta lagche");
+
+
+document.getElementById('btn-login').addEventListener('click', function(event){
+    event.preventDefault()
+    // console.log('log in btn clicked');
+
+    const phoneNumber = document.getElementById('mobile-number').value;
+    console.log(phoneNumber);
+
+})
