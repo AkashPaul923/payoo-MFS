42247726ce398c4b87c8e0b7f019b0c18b3ff18f
AkashPaul923 <akashpaul923@gmail.com>
2024-09-19 13:30:41 +0600

    add money in current balance

--- js/home.js
+++ js/home.js
@@ -1,1 +1,18 @@
 // console.log('hello');
+// add money
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault()
+
+    const addMoney = document.getElementById('add-money').value
+    const addMoneyPin = document.getElementById('add-money-pin').value
+    const balance = document.getElementById('available-balance').innerText
+
+    if(addMoneyPin === '1234'){
+        const updateBalance = parseFloat(addMoney) + parseFloat(balance)
+        // console.log(updateBalance);
+        document.getElementById('available-balance').innerText = updateBalance
+    }
+    else{
+        alert('wrong pin number')
+    }
+})
