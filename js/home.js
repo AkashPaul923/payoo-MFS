2657cf233ec338a72d578a50c779ba7e33a43f62
AkashPaul923 <akashpaul923@gmail.com>
2024-09-20 19:28:49 +0600

    add money and cash out using function

--- js/home.js
+++ js/home.js
@@ -1,18 +1,78 @@
 // console.log('hello');
 // add money
+// document.getElementById('btn-add-money').addEventListener('click', function(event){
+//     event.preventDefault()
+
+//     const addMoney = document.getElementById('add-money').value
+//     const addMoneyPin = document.getElementById('add-money-pin').value
+//     const balance = document.getElementById('available-balance').innerText
+
+//     if(addMoneyPin === '1234'){
+//         const updateBalance = parseFloat(addMoney) + parseFloat(balance)
+//         // console.log(updateBalance);
+//         document.getElementById('available-balance').innerText = updateBalance
+//     }
+//     else{
+//         alert('wrong pin number')
+//     }
+// })
+
+// document.getElementById('btn-cash-out').addEventListener('click', function(event){
+//     event.preventDefault()
+
+//     const cashOut = document.getElementById('cash-out').value
+//     const cashOutPin = document.getElementById('cash-out-pin').value
+//     const balance = document.getElementById('available-balance').innerText
+
+//     if(cashOutPin === '1234'){
+//         const updateBalance =  parseFloat(balance) - parseFloat(cashOut)
+//         // console.log(updateBalance);
+//         document.getElementById('available-balance').innerText = updateBalance
+//     }
+//     else{
+//         alert('wrong pin number')
+//     }
+// })
+
+
+
+
+// add money
 document.getElementById('btn-add-money').addEventListener('click', function(event){
     event.preventDefault()
 
-    const addMoney = document.getElementById('add-money').value
-    const addMoneyPin = document.getElementById('add-money-pin').value
-    const balance = document.getElementById('available-balance').innerText
+    const addMoney = getInputFieldValueById('add-money-input')
+    const addMoneyPin = getInputFieldValueById('add-money-pin')
+    const balance = getTextFieldValueById('available-balance')
 
-    if(addMoneyPin === '1234'){
-        const updateBalance = parseFloat(addMoney) + parseFloat(balance)
+    if(addMoneyPin === 1234){
+        const updateBalance = balance + addMoney
         // console.log(updateBalance);
         document.getElementById('available-balance').innerText = updateBalance
     }
     else{
         alert('wrong pin number')
     }
+    console.log(addMoney , addMoneyPin , balance) ;
+})
+
+
+
+// cash out
+document.getElementById('btn-cash-out').addEventListener('click', function(event){
+    event.preventDefault()
+
+    const cashOut = getInputFieldValueById('cash-out-input')
+    const cashOutPin = getInputFieldValueById('cash-out-pin')
+    const balance = getTextFieldValueById('available-balance')
+
+    if(cashOutPin === 1234){
+        const updateBalance = balance - cashOut
+        // console.log(updateBalance);
+        document.getElementById('available-balance').innerText = updateBalance
+    }
+    else{
+        alert('wrong pin number')
+    }
+    
 })
--- js/login.js
+++ js/login.js
@@ -1,17 +1,17 @@
 
 
 
-document.getElementById('btn-login').addEventListener('click', function(event){
+document.getElementById('btn-login').addEventListener('click', function (event) {
     event.preventDefault()
     // console.log('log in btn clicked');
 
     const phoneNumber = document.getElementById('mobile-number').value;
     const pinNumber = document.getElementById('pin-number').value
 
-    if(phoneNumber === "01500000000" && pinNumber === "1234"){
+    if (phoneNumber === "01500000000" && pinNumber === "1234") {
         window.location.href = '/home.html'
     }
-    else{
+    else {
         alert('wrong mobile number or pin')
     }
 
